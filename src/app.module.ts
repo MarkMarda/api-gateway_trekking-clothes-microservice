1ec0380fff05fa315fcc853af6d0e9444029104a
Marco A. R. del A <marda_da-capo@hotmail.com>
2024-07-21 14:36:20 -0600

    feat: Enabling DataBase configuration, CORS, global validation, openApi Spec Configuration, versioning the api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,33 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CommonModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    //To stablish the environment variables
+    ConfigModule.forRoot(),
+
+    //DataBase configuration
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, //False in production
+      extra: {
+        connectionTimeoutMillis: 30000, // 30 segundos
+      },
+      logging: 'all',
+    }),
+
+    CommonModule,
+
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Enable CORS
+  app.enableCors();
+
+  const logger = new Logger('Bootstrap');
+
+  //Versioning the API
+  app.setGlobalPrefix('api/v1');
+
+  //Config global validation
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  //Config openApi Specification
+  const config = new DocumentBuilder()
+    .setTitle('API Gateway Microservice')
+    .setDescription('API Gateway Microservice')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT);
+
+  logger.log(`Application listening on port ${process.env.PORT}`);
 }
 bootstrap();
